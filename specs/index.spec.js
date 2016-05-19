0b5a4f8c50435ba6df2c36e5c23d5eda6e9356cb
Vlad Rindevich <rindevich.vs@gmail.com>
2016-05-19 23:58:01 +0300

    add: asynchronous processing

--- specs/index.spec.js
+++ specs/index.spec.js
@@ -1,5 +1,5 @@
 import {expect} from 'chai';
-import {readFileSync} from 'fs';
+import {readFile, readFileSync} from 'fs-promise';
 import {join} from 'path';
 import {Builder} from 'broccoli';
 import Babel from '../src';
@@ -8,9 +8,11 @@ describe('Babel broccoli plugin', () => {
   it('should transpile es6 files to es5', () => {
     const nodes = new Babel(join(__dirname, 'fixtures'), {presets: 'es2015'});
 
-    return (new Builder(nodes)).build().then(() => {
-      const fixture = readFileSync(join(nodes.outputPath, 'basic-compile.js'), 'utf8');
-      const expectation = readFileSync(join(__dirname, 'expects', 'basic-compile.js'), 'utf8');
+    return (new Builder(nodes)).build().then(async() => {
+      const fixturePath = join(nodes.outputPath, 'basic-compile.js');
+      const fixture = await readFile(fixturePath, 'utf8');
+      const expectsPath = join(__dirname, 'expects', 'basic-compile.js');
+      const expectation = await readFile(expectsPath, 'utf8');
 
       expect(fixture).to.equal(expectation);
     });
@@ -21,7 +23,7 @@ describe('Babel broccoli plugin', () => {
 
     return (new Builder(nodes)).build().then(() => {
       expect(() => {
-        readFileSync(join(nodes.outputPath, 'basic-not-compile.html'), 'utf8')
+        readFileSync(join(nodes.outputPath, 'basic-not-compile.html'), 'utf8');
       }).to.throw(Error);
     });
   });
@@ -31,5 +33,13 @@ describe('Babel broccoli plugin', () => {
       new Babel([join(__dirname, 'fixtures'), join(__dirname, 'expects')]);
       new Babel(join(__dirname, 'fixtures'));
     }).to.not.throw(TypeError);
+  });
+
+  it('should remove broccoli plugin options from babel option list', () => {
+    const babel = new Babel(join(__dirname, 'fixtures'), {
+      persistentOutput: true
+    });
+
+    expect(babel.options.persistentOutput).to.be.undefined;
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,34 +1,40 @@
-import {writeFileSync} from 'fs';
-import {transformFileSync} from 'babel-core';
-import {dirname} from 'path';
-import {sync as createDirSync} from 'mkdirp';
-import Plugin from 'broccoli-plugin';
-import walkSync from 'walk-sync';
 import 'babel-polyfill';
+import {readdir, readFile, writeFile} from 'fs-promise';
+import {transform} from 'babel-core';
+import {dirname} from 'path';
+import mkdirp from 'mkdirp-promise';
+import Plugin from 'broccoli-plugin';
 
 export default class Babel extends Plugin {
   constructor(inputNodes, options = {}) {
     super(Array.isArray(inputNodes) ? inputNodes : [inputNodes], {
-      annotation: options.annotation
+      name: 'Babel',
+      annotation: 'ES6 to ES5 transpilation plugin for Broccoli',
+      persistentOutput: options.persistentOutput ?
+          options.persistentOutput :
+          false
     });
+
+    if (options.persistentOutput) {
+      delete options.persistentOutput;
+    }
 
     this.options = options;
   }
 
-  build() {
-    const srcDir = this.inputPaths[0];
+  async build() {
+    const [inputPath] = this.inputPaths;
 
-    for (let filename of walkSync(srcDir)) {
+    for (let filename of await readdir(inputPath)) {
       if (filename.slice(-3) !== '.js') {
         continue;
       }
 
-      const output = transformFileSync(`${srcDir}/${filename}`, this.options);
+      const code = await readFile(`${inputPath}/${filename}`);
+      const output = transform(code, this.options);
       const outputFilename = `${this.outputPath}/${filename}`;
-
-      createDirSync(dirname(outputFilename));
-
-      writeFileSync(outputFilename, output.code);
+      await mkdirp(dirname(outputFilename));
+      await writeFile(outputFilename, output.code);
     }
   }
 }
